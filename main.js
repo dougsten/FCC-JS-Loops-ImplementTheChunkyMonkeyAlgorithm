7e37aaa2908aa766dec3bd7ad1cf153a5da2c8fa
Thunder380 <76134812+Thunder380@users.noreply.github.com>
2025-09-18 11:17:49 -0700

    passes all tests

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 function chunkArrayInGroups(arr, size) {
   const newArr = [];
   for (let i = 0; i < arr.length; i += size) {
-    newArr.push(arr.slice(i, i + size));
+    newArr.push(arr.slice(i, i + size)); 
   }
   return newArr;
 };
+
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3))
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2))
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4))
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3))
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4))
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2))
+
